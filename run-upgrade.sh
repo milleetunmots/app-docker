b4b1d8ccf2db3b02716e792382e0101a7d1e0fb4
Fabien Karquel <fabien.karquel@gmail.com>
2023-02-13 17:05:23 +0100

    clean unused docker images after deploying

--- run-upgrade.sh
+++ run-upgrade.sh
@@ -10,3 +10,4 @@ cd "$(dirname "$0")"
 /usr/bin/docker-compose down --remove-orphans
 /usr/bin/docker volume rm -f docker_rails_public
 /usr/bin/docker-compose up -d
+/usr/bin/docker image prune -af
